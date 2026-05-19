ceadff914d3c671b9a158c17547971f5bb700162
Pranav rajput <118982987+pranav0043@users.noreply.github.com>
2026-05-19 09:08:29 +0530

    Update output messages in Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("pranav world");
+        System.out.println("pranav");
+        System.out.println("100 Days of coding Challenge");
     }
 
 }
